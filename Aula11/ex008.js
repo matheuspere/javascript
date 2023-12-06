bf10454bfdaf9abf915c9f04839388dd3dcdcdd1
Matheus Pereira <128595942+matheuspere@users.noreply.github.com>
2023-12-06 09:55:23 -0300

    Atualizei

---
+++ Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 50) {
+    console.log(`Você ultrapassou a velocidade permitida. Multado!!!`)
+}
+
+console.log(`Dirija usando cinto de segurança`)
