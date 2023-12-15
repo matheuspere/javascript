344b52083c2b6b3c7fd396de193e63f705f8ac9c
Matheus Pereira <128595942+matheuspere@users.noreply.github.com>
2023-12-15 19:09:22 -0300

    Atualizei Aula 14

---
+++ Aula13/ambiente.js
@@ -0,0 +1,28 @@
+
+for (var c=1;c<=10;c++);
+
+
+/*
+var c = 1
+do {console.log(`Passo ${c}`)
+c++}
+while (c <=25) {
+    
+}
+*/
+
+
+
+
+
+/*
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+*/
+
---
+++ Aula14/ambiente.js
@@ -0,0 +1,11 @@
+/*
+var c = 1
+while (c <= 6) {
+    console.log(c)
+    c++
+}*/
+
+
+for(var c = 1;c <= 5;c++){
+    console.log(c)
+}
