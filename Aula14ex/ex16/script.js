92d547147e95383110c7510ec9fec06331e6109f
Matheus Pereira <128595942+matheuspere@users.noreply.github.com>
2023-12-22 08:16:56 -0300

    atualizei

--- Aula14ex/ex16/script.js
+++ Aula14ex/ex16/script.js
@@ -1,28 +1,25 @@
-function Contar(){
-    let ini = document.getElementById('txtini')
-    let fim = document.getElementById('txtfim')
-    let passo = document.getElementById('txtpas')
-    let res = document.getElementById('res')
-
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[Erro] Faltam dados!')
-    } else {
-       res.innerHTML = 'Contando: '
-       let i = Number(ini.value)
-       let f = Number(fim.value)
-       let p = Number(passo.value)
-       if (i < f) {
-        for(let c = i; c <= f; c+= p ) {
-            res.innerHTML += `${c} \u{27A1}`
-       }
-        res.innerHTML += `\u{1F51A}`
-       }
-          else {
-            for(let c = i; c >= f, c -= p) {
-                res.innerHTML += ` ${c} \u{27A1}`
-            }
-            
-         }
-         res.innerHTML += `\u{1F51A}`
-}
-}
+function contar() {
+    let inicio = document.getElementById ("txti")
+    let fim = document.getElementById ('txtf')
+    let passo = document.getElementById('txtp')
+    let res= document.getElementById('res')
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length==0){
+        alert('Erro - verifique os dados!')}
+        else { 
+            res.innerHTML = 'Contando: <br>'
+            let i = Number(inicio.value)
+            let f = Number(fim.value)
+            let p = Number(passo.value)
+        if (p <= 0){alert( 'Passo invalido! Considerando passo 1'); p = 1 }
+            if (i<=f) {
+                //contando crescente
+                for (let c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449}`}
+            } else {
+                //contagem regressiva
+                for (let c = i; c >= f; c -= p) {  res.innerHTML += `${c} \u{1F449}`
+           
+                }
+            }  res.innerHTML += ` ${c}`
+        }
+    }
