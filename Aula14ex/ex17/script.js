d49c71a6c83b848827269a1d7f532e215c1c4107
Matheus Pereira <128595942+matheuspere@users.noreply.github.com>
2023-12-23 14:04:37 -0300

    autalizei

--- Aula14ex/ex17/script.js
+++ Aula14ex/ex17/script.js
@@ -1,1 +1,18 @@
-alert('Olá,mundo"')
+function gerartabuada() {
+let num = document.getElementById('txtn')
+let tab = document.getElementById('seltab')
+
+if (num.value.length == 0) {
+    window.alert ('Por Favor, digite um número!')
+} else {
+    let n = Number(num.value)
+    let c = 1
+    while (c <=10)
+    item = document.createElement('option')
+    item.text = `${n} x ${c} = ${n*c}`
+    tab.appendChild(item)
+    c++
+    
+}
+
+}
