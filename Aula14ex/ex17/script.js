d2f7540628500d47c6e28bc7642fe8a2e7bb2fdd
Matheus Pereira <128595942+matheuspere@users.noreply.github.com>
2023-12-26 20:27:51 -0300

    tabuada

--- Aula14ex/ex17/script.js
+++ Aula14ex/ex17/script.js
@@ -1,18 +1,18 @@
-function gerartabuada() {
-let num = document.getElementById('txtn')
-let tab = document.getElementById('seltab')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
 
-if (num.value.length == 0) {
-    window.alert ('Por Favor, digite um número!')
-} else {
-    let n = Number(num.value)
-    let c = 1
-    while (c <=10)
-    item = document.createElement('option')
-    item.text = `${n} x ${c} = ${n*c}`
-    tab.appendChild(item)
-    c++
-    
-}
+    if (num.value.length == 0) {
+        window.alert('Por Favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = '' // Limpa a tabela antes de gerar a nova tabuada
 
+        for (let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
 }
