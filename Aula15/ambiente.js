2d7c8b3bac0a1b76446a5a54d81181f77c9b9540
Matheus Pereira <128595942+matheuspere@users.noreply.github.com>
2024-01-08 14:18:46 -0300

    subi

---
+++ Aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [ 5, 8, 2, 9, 3]
+
+num.sort()
+console.log(num)
+console.log(`Nosso vetor é o ${num.length} posições`)
+console.log(`O priemiro valor valor do vetor é ${num[1]}`)
---
+++ Aula16/função03.js
@@ -0,0 +1,5 @@
+let v = function(x) {
+    return x*9
+}
+
+console.log(v(5))
